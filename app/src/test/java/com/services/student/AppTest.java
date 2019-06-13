050ff4f34eb2322e8a3ce6de96796208d372e043
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-13 01:03:37 +0100

    Added test to test pipeline

---
+++ app/src/test/java/com/services/student/AppTest.java
@@ -0,0 +1,15 @@
+package com.services.student;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTest {
+
+
+    @Test
+    public void someTest() {
+
+    }
+}
