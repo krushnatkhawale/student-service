7759af0efa90d9e4cb7c50ecdc386925b245b414
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-21 23:16:22 +0100

    QA: Added cucumber dependencies

---
+++ qa/src/test/java/PostStudentStepDefs.java
@@ -0,0 +1,31 @@
+import com.services.student.RestClient;
+import com.services.student.model.Student;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.http.HttpStatus.OK;
+
+public class PostStudentStepDefs {
+    private Student student;
+    private RestClient client;
+    private Student retrievedStudent;
+    private ResponseEntity<Student> postStudentResponseEntity;
+
+    @Given("^A student is ready$")
+    public void aStudentIsReady() {
+        student = new Student();
+    }
+
+    @When("^post endpoint is hit$")
+    public void postEndpointIsHit() {
+        postStudentResponseEntity = client.postStudent(student);
+    }
+
+    @Then("^a student profile is created$")
+    public void aStudentProfileIsCreated() {
+        assertEquals(postStudentResponseEntity.getStatusCode(), OK);
+    }
+}
---
+++ qa/src/test/java/com/services/student/RestClient.java
@@ -0,0 +1,16 @@
+package com.services.student;
+
+import com.services.student.model.Student;
+import org.springframework.http.ResponseEntity;
+
+public class RestClient {
+
+    public ResponseEntity<Student> postStudent(Student student) {
+
+        return null;
+    }
+
+    public Student getStudent(String id) {
+        return null;
+    }
+}
