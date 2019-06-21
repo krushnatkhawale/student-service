9130e3b45eabf7065b9dcd6d272d55cd3b0c93d0
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-21 23:59:11 +0100

    QA: put steps file added

---
+++ qa/src/test/java/com/services/student/steps/PutStepsDefs.java
@@ -0,0 +1,9 @@
+package com.services.student.steps;
+
+import cucumber.api.java.en.When;
+
+public class PutStepsDefs {
+    @When("put endpoint is hit")
+    public void putEndpointIsHit() {
+    }
+}
