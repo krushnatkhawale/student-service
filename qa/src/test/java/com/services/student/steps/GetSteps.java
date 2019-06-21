46a02900af2c5f589a89b9aed44d2a4f10289f61
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-21 23:58:42 +0100

    QA: delete steps file added

---
+++ qa/src/test/java/com/services/student/steps/GetSteps.java
@@ -0,0 +1,9 @@
+package com.services.student.steps;
+
+import cucumber.api.java.en.Then;
+
+public class GetSteps {
+    @Then("a student profile is retrieved")
+    public void aStudentProfileIsRetrieved() {
+    }
+}
