02778bbc35c2e8ceaaddc1235fe716cdfa2ed265
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-25 00:10:46 +0100

    QA: Cucumber is working now, yes really working.

--- qa/src/main/java/com/services/student/model/Student.java
+++ qa/src/main/java/com/services/student/model/Student.java
@@ -1,4 +1,14 @@
 package com.services.student.model;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String id;
 }
