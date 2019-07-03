f2d08badbdbf1a579de6b282493d18196ca93bed
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-03 23:09:13 +0100

    QA: Lombok issue fix test

--- app/src/main/java/com/services/student/model/Student.java
+++ app/src/main/java/com/services/student/model/Student.java
@@ -1,12 +1,10 @@
 package com.services.student.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class Student {
     private String firstName;
     private String lastName;
