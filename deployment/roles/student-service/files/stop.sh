e4f4bef39945c945d0145d9150e40f761fbf5d25
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-14 10:50:09 +0100

    Ansible: forful file cleanup

--- deployment/roles/student-service/files/stop.sh
+++ deployment/roles/student-service/files/stop.sh
@@ -1,5 +1,5 @@
 sudo kill -9 $(cat student-service.pid)
 
-sudo echo "App $(cat student-service.pid) killed at $(date)" > student-service.deleted
+echo "App $(cat student-service.pid) killed at $(date)" > student-service.deleted
 
-sudo rm  -f student-service.pid
+rm  -f student-service.pid
