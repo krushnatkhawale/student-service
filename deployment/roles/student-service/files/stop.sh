89aa77c2dc936f40201d4d2ad0341cd22dc09226
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-14 11:31:38 +0100

    Ansible: refactoring

--- deployment/roles/student-service/files/stop.sh
+++ deployment/roles/student-service/files/stop.sh
@@ -1,5 +1,5 @@
 sudo kill -9 $(cat student-service.pid)
 
-echo "App $(cat student-service.pid) killed at $(date)" > student-service.deleted
+echo "App with pid [$(cat student-service.pid)] killed at $(date)" > student-service.deleted
 
 rm  -f student-service.pid
