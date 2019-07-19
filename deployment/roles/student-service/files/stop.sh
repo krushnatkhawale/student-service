02518ab828c79fe41d406df897b6e9817bbde281
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-20 00:02:51 +0100

    Ansible: stop.sh cat-ing contents to .deleted file

--- deployment/roles/student-service/files/stop.sh
+++ deployment/roles/student-service/files/stop.sh
@@ -1,1 +1,15 @@
- kill -9 $(cat student-service.pid)
+echo "\n Processes before shutdown:\n" > student-service.deleted
+
+ps -ef | grep student-service.jar >> student-service.deleted
+
+echo "\n Processe pid before shutdown:\n" >> student-service.deleted
+
+ps -auxww | grep $(cat student-service.pid) >> student-service.deleted
+
+kill -9 $(cat student-service.pid)
+
+rm student-service.pid
+
+echo "\n Processes after shutdown:\n" >> student-service.deleted
+
+ps -ef | grep student-service.jar >> student-service.deleted
