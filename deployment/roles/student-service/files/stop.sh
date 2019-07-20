a06ac7a17a0893c3dced381144eec27cf1179875
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-20 01:14:51 +0100

    Ansible: sudo kill used

--- deployment/roles/student-service/files/stop.sh
+++ deployment/roles/student-service/files/stop.sh
@@ -1,15 +1,15 @@
-echo "\n Processes before shutdown:\n" > student-service.deleted
+echo "\nProcesses before shutdown:\n" > student-service.deleted
 
 ps -ef | grep student-service.jar >> student-service.deleted
 
-echo "\n Processe pid before shutdown:\n" >> student-service.deleted
+echo "\nProcesse pid before shutdown:\n" >> student-service.deleted
 
 ps -auxww | grep $(cat student-service.pid) >> student-service.deleted
 
-kill -9 $(cat student-service.pid)
+sudo kill -9 $(cat student-service.pid)
 
 rm student-service.pid
 
-echo "\n Processes after shutdown:\n" >> student-service.deleted
+echo "\nProcesses after shutdown:\n" >> student-service.deleted
 
 ps -ef | grep student-service.jar >> student-service.deleted
