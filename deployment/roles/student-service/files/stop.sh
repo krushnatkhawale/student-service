1df1d6672692faa893c3f9cbde14f63d7cf7df8c
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-20 01:20:29 +0100

    Ansible: stop.sh log messages added

--- deployment/roles/student-service/files/stop.sh
+++ deployment/roles/student-service/files/stop.sh
@@ -6,6 +6,8 @@ echo "\nProcesse pid before shutdown:\n" >> student-service.deleted
 
 ps -auxww | grep $(cat student-service.pid) >> student-service.deleted
 
+echo "\nKilling process with pid - $(cat student-service.pid)\n"
+
 sudo kill -9 $(cat student-service.pid)
 
 rm student-service.pid
