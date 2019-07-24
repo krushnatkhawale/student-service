38ee441250bf48151c4832b0dbfe1b84b5d66a1e
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-24 21:59:11 +0100

    PerfTest: rest client added

---
+++ perfTest/src/test/java/com/services/student/model/Student.java
@@ -0,0 +1,49 @@
+package com.services.student.model;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String id;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
