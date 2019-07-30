89a74e1d72dc0868761d2ae8f4db075f4656fc6f
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-30 23:17:18 +0100

    performance-test: Refactor + delete method added to RestClient

--- performance-test/src/test/java/com/services/student/client/RestClient.java
+++ performance-test/src/test/java/com/services/student/client/RestClient.java
@@ -6,23 +6,28 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
 import java.util.List;
 
 public class RestClient {
 
     private RestTemplate restTemplate;
-    private static final String POST_URL = "/students";
+    private static final String BASE_URL = "/students";
 
     public RestClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public ResponseEntity<Void> postStudent(Student student) {
-        return restTemplate.postForEntity(POST_URL, student, Void.class);
+        return restTemplate.postForEntity(BASE_URL, student, Void.class);
     }
 
     public ResponseEntity<List<Student>> getAllStudents() {
-        return restTemplate.exchange(POST_URL, HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {
+        return restTemplate.exchange(BASE_URL, HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {
         });
+    }
+
+    public void deleteStudent(String id) {
+        restTemplate.delete(BASE_URL + File.separator + id);
     }
 }
